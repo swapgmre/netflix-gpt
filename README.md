6b052e7a219c1c36403db4cb4d4d81df4f83336f
Swapnil Gamre <swapgmre@gmail.com>
2025-06-17 21:32:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Features
+
+-Login/Signup
+-Sign In/Sign Up form
+-Redirect to browse page
+
+- Browse (after authentication)
+  -Header
+  -Main Movie
+  --Trailer in the background
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500 font-bold">Netflix Gpt</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
